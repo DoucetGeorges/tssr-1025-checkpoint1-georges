17e8095719df8a473e44fcbc2e0449ab33f91b11
Doucet Georges <maisondoucet@gmail.com>
2025-11-23 13:14:08 +0100

    Update addUsers.sh

--- addUsers.sh
+++ addUsers.sh
@@ -1,46 +1,34 @@
 #!/bin/bash
 
-if [ "$#" -eq 0 ]; then
 
-    echo "Il manque les noms d'utilisateurs en argument - Fin du script"
+#Vérification de présence d'arguments
+if [ $# = 0 ];then
+    echo " Il manque les noms d'utilisateurs en argument - Fin du script "
     exit 1
 fi
 
+#début de boucle pour traiter tout les utilisateurs
+for utilisateur in "$@"
+do
 
-if [ -f "$1"  ]; then
-    echo "L'utilisateur "$1" existe déjà"
-else 
-    sudo useradd "$1"
-fi
+#Vérification de l'existence des utilisateurs et affichage d'un message en conséquence
+    if [ "$utilisateur" > /dev/null 2>&1 ];then
+    echo "l'utilisateur  $utilisateur existe déjà"
 
-if [ -f "$2"  ]; then
-    echo "L'utilisateur "$2" existe déjà" 
-else
-    sudo useradd "$2"
-fi
+    else 
+    echo "l'utilisateur $utilisateur n'existe pas"
+    fi
+#Création des utilisateurs
+    useradd "$utilisateur"
+#Affichage du résultats
+    if [ $? -eq 0 ];then
+    echo " l'utilisateur $utilisateur a bien été crée "
 
-if [ -f "$3"  ]; then
-    echo "L'utilisateur "$3" existe déjà" 
-else
-    sudo useradd "$3"
-fi
-
-if [ -f "$1"  ]; then
-    echo "L'utilisateur $1 a été crée"
-else
-    echo "Erreur à la création de l'utilisateur $1"
-fi
-
-if [ -f "$2"  ]; then
-    echo "L'utilisateur $2 a été crée"
-else
-    echo "Erreur à la création de l'utilisateur $2"
-fi
+    else
+    echo "Erreur à la création de l'utilisateur $utilisateur"
+   fi
+#fin de boucle
+done
 
 
-if [ -f "$3"  ]; then
-    echo "L'utilisateur $3 a été crée"
-else
-    echo "Erreur à la création de l'utilisateur $3"
-fi
 
